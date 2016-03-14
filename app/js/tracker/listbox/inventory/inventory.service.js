c7383d67387f4132491ad4b76efaabf3835d823e
Alex Sherbuck <Spactigus@gmail.com>
2016-03-14 07:01:58 -0400

    added armor and weapon data

--- app/js/tracker/listbox/inventory/inventory.service.js
+++ app/js/tracker/listbox/inventory/inventory.service.js
@@ -49,8 +49,33 @@
     ];
     
     var armors = [
-      
+      {name: 'Padded', img: '', desc: '', slot: ['torso'], equipable: true, type: 'light', cost: 5, armor: 11, maxdex: 100, strreq: 0, weight: 8, stealthdis: true},
+      {name: 'Leather', img: '', desc: '',  slot: ['torso'], equipable: true, type: 'light', cost: 10, armor: 11, maxdex: 100, strreq: 0, weight: 10, stealthdis: false},
+      {name: 'Studded Leather', img: '', desc: '',  slot: ['torso'], equipable: true,  type: 'light', cost: 45, armor: 12, maxdex: 100, strreq: 0, weight: 13, stealthdis: false},
+      {name: 'Hide', img: '', desc: '',  slot: ['torso'], equipable: true,  type: 'medium', cost: 10, armor: 12, maxdex: 2, strreq: 0, weight: 12, stealthdis: false},
+      {name: 'Chain Shirt', img: '', desc: '',  slot: ['torso'], equipable: true,  type: 'medium', cost: 50, armor: 13, maxdex: 2, strreq: 0, weight: 20, stealthdis: false},
+      {name: 'Scale Mail', img: '', desc: '',  slot: ['torso'], equipable: true,  type: 'medium', cost: 50, armor: 14, maxdex: 2, strreq: 0, weight: 45, stealthdis: true},
+      {name: 'Breastplate', img: '', desc: '',  slot: ['torso'], equipable: true,  type: 'medium', cost: 400, armor: 14, maxdex: 2, strreq: 0, weight: 20, stealthdis: false},
+      {name: 'Half Plate', img: '', desc: '',  slot: ['torso'], equipable: true,  type: 'medium', cost: 750, armor: 15, maxdex: 2, strreq: 0, weight: 40, stealthdis: true},
+      {name: 'Ring Mail', img: '', desc: '',  slot: ['torso'], equipable: true,  type: 'heavy', cost: 30, armor: 14, maxdex: 0, strreq: 0, weight: 40, stealthdis: true},
+      {name: 'Chain Mail', img: '', desc: '',  slot: ['torso'], equipable: true,  type: 'heavy', cost: 75, armor: 16, maxdex: 0, strreq: 13, weight: 55, stealthdis: true},
+      {name: 'Splint', img: '', desc: '',  slot: ['torso'], equipable: true,  type: 'heavy', cost: 200, armor: 17, maxdex: 0, strreq: 15, weight: 60, stealthdis: true},
+      {name: 'Plate', img: '', desc: '',  slot: ['torso'], equipable: true,  type: 'heavy', cost: 1500, armor: 18, maxdex: 0, strreq: 15, weight: 65, stealthdis: true},
+      {name: 'shield', img: '', desc: '',  slot: ['torso'], equipable: true,  type: 'shield', cost: 10, armor: 2, maxdex: 0, strreq: 15, weight: 65, stealthdis: true},
     ];
+    
+    var weapons = [
+      {name: 'Club', img: '', desc: '', slot: ['weapon1', 'weapon2'], equipable: true, type: 'simple', cost: 1, damage: 4, damagetype: 'bludgeoning', weight: 2, properties: ['light']},
+      {name: 'Dagger', img: '', desc: '', slot: ['weapon1', 'weapon2', 'ranged'], equipable: true, type: 'simple', cost: 2, damage: 4, damagetype: 'piercing', weight: 1, properties: ['light', 'finesse', 'thrown']},
+      {name: 'Greatclub', img: '', desc: '', slot: ['2h'], equipable: true, type: 'simple', cost: 2, damage: 8, damagetype: 'bludgeoning', weight: 10, properties: ['two-handed']},
+      {name: 'Handaxe', img: '', desc: '', slot: ['weapon1', 'weapon2', 'ranged'], equipable: true, type: 'simple', cost: 5, damage: 6, damagetype: 'slashing', weight: 2, properties: ['light', 'thrown']},
+      {name: 'Javelin', img: '', desc: '', slot: ['weapon1', 'weapon2', 'ranged'], equipable: true, type: 'simple', cost: 5, damage: 6, damagetype: 'piercing', weight: 2, properties: ['thrown']},
+      {name: 'Light Hammer', img: '', desc: '', slot: ['weapon1', 'weapon2', 'ranged'], equipable: true, type: 'simple', cost: 2, damage: 4, damagetype: 'bludgeoning', weight: 2, properties: ['light', 'thrown']},
+      {name: 'Mace', img: '', desc: '', slot: ['weapon1', 'weapon2'], equipable: true, type: 'simple', cost: 5, damage: 6, damagetype: 'bludgeoning', weight: 4, properties: []},
+      {name: 'Quarterstaff', img: '', desc: '', slot: ['weapon1', 'weapon2'], equipable: true, type: 'simple', cost: 2, damage: 6, damagetype: 'bludgeoning', weight: 4, properties: ['versatile']},
+      {name: 'Sickle', img: '', desc: '', slot: ['weapon1', 'weapon2'], equipable: true, type: 'simple', cost: 1, damage: 4, damagetype: 'slashing', weight: 2, properties: ['light']},
+      {name: 'Spear', img: '', desc: '', slot: ['weapon1', 'weapon2', 'ranged'], equipable: true, type: 'simple', cost: 1, damage: 6, damagetype: 'piercing', weight: 3, properties: ['versatile', 'thrown']},
+    ]
     
     return {
       equipItem: equipItem,
